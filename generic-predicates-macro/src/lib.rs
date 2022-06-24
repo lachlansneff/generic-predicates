18f580843350ef854f381dd50ffb73afb7f0a983
Lachlan Sneff <lachlan.sneff@gmail.com>
2022-06-24 00:07:21 -0700

    Add docs to proc macro

--- generic-predicates-macro/src/lib.rs
+++ generic-predicates-macro/src/lib.rs
@@ -479,6 +479,29 @@ impl Parse for ItemFn {
     }
 }
 
+/// 
+/// ```rust,no_run
+/// #![feature(generic_const_exprs)]
+///
+/// use generic_predicates::generic_predicates;
+///
+/// generic_predicates! {
+///     pub fn foo<const N: usize>()
+///     where
+///         (N > 23, "`N` must be greater than 23")
+///     {
+///
+///     }
+/// }
+///
+/// fn main() {
+///     // This compiles.
+///     foo::<24>();
+///
+///     // This doesn't.
+///     foo::<23>();
+/// }
+/// ```
 #[proc_macro]
 pub fn generic_predicates(item: TokenStream) -> TokenStream {
     let mut func = parse_macro_input!(item as ItemFn);
